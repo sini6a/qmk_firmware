a5e52a2a7ea53102009849ff0f2d64bee6f612f5
Sinisha Stojchevski <stojchevski0@gmail.com>
2025-05-08 18:34:05 +0200

    replaced kc_comma with kc_quote because its missing

--- keyboards/phnx/keymaps/via/keymap.c
+++ keyboards/phnx/keymaps/via/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,             KC_NO,
         KC_NO,     KC_LBRC,   KC_7,   KC_8,   KC_9,   KC_RCBR,         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,             KC_NO,
         KC_NO,     KC_SCLN,   KC_4,   KC_5,   KC_6,   KC_EQL,         KC_NO,   KC_RSFT,   KC_RCTL,   KC_RALT,   KC_RGUI,     KC_NO,
-        KC_NO,     KC_COMM,   KC_1,   KC_2,   KC_3,   KC_BSLS,         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,
+        KC_NO,     KC_QUOTE,   KC_1,   KC_2,   KC_3,   KC_BSLS,         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,
                                          KC_DOT, KC_0, KC_MINUS,         KC_NO, KC_NO, KC_NO
     ),
     [4] = LAYOUT_qwerty( // symbol layer
